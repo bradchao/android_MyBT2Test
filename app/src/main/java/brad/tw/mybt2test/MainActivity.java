f7cd2e5997d1d04ee586ac3a59fb0c9d894a75da
Brad Chao <brad@brad.tw>
2016-09-30 10:02:32 +0800

    v2

--- app/src/main/java/brad/tw/mybt2test/MainActivity.java
+++ app/src/main/java/brad/tw/mybt2test/MainActivity.java
@@ -1,13 +1,42 @@
 package brad.tw.mybt2test;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private BluetoothAdapter mBluetoothAdapter;
+    private boolean isSupport = true;
+    private boolean isBTInitEnable = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupBT();
+    }
+
+    public void setupBT(){
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+            isSupport = false;
+        }else {
+            if (!mBluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(
+                        BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, 1);
+            }else{
+                isBTInitEnable = true;
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
     }
 }
