a364fe9d10a998d096fa83e9dc71fea6652319af
Brad Chao <brad@brad.tw>
2016-09-30 13:41:13 +0800

    v12

--- app/src/main/java/brad/tw/mybt2test/MainActivity.java
+++ app/src/main/java/brad/tw/mybt2test/MainActivity.java
@@ -74,7 +74,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 BluetoothDevice remoteDevice = (BluetoothDevice) data.get(position).get("device");
-                
+                ConnectThread clientThread = new ConnectThread(remoteDevice);
+                clientThread.start();
             }
         });
 
@@ -234,6 +235,53 @@ public class MainActivity extends AppCompatActivity {
         public void cancel() {
             try {
                 mmServerSocket.close();
+            } catch (IOException e) { }
+        }
+    }
+
+    private class ConnectThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final BluetoothDevice mmDevice;
+
+        public ConnectThread(BluetoothDevice device) {
+            // Use a temporary object that is later assigned to mmSocket,
+            // because mmSocket is final
+            BluetoothSocket tmp = null;
+            mmDevice = device;
+
+            // Get a BluetoothSocket to connect with the given BluetoothDevice
+            try {
+                // MY_UUID is the app's UUID string, also used by the server code
+                tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+            } catch (IOException e) { }
+            mmSocket = tmp;
+        }
+
+        public void run() {
+            // Cancel discovery because it will slow down the connection
+            mBluetoothAdapter.cancelDiscovery();
+
+            try {
+                // Connect the device through the socket. This will block
+                // until it succeeds or throws an exception
+                mmSocket.connect();
+                Log.d("brad", "Client connect OK");
+            } catch (IOException connectException) {
+                // Unable to connect; close the socket and get out
+                try {
+                    mmSocket.close();
+                } catch (IOException closeException) { }
+                return;
+            }
+
+            // Do work to manage the connection (in a separate thread)
+            //manageConnectedSocket(mmSocket);
+        }
+
+        /** Will cancel an in-progress connection, and close the socket */
+        public void cancel() {
+            try {
+                mmSocket.close();
             } catch (IOException e) { }
         }
     }
